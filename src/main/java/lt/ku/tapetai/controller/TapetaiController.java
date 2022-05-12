606d244426f5dc89f49d7aea7087c1a37c903286
Dangyra <dangyra.tadaraite@gmail.com>
2022-05-12 23:32:19 +0300

    Skaičiuoklė

--- src/main/java/lt/ku/tapetai/controller/TapetaiController.java
+++ src/main/java/lt/ku/tapetai/controller/TapetaiController.java
@@ -4,31 +4,46 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/")
 public class TapetaiController {
 
-		@GetMapping("/")
-		public String forma(){
-			return "forma";
-		}
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+	
+	@PostMapping("/")
+	public String homee() {
+		return home();
+	}
 
-		@PostMapping("/")
-		public String pvm(
-				@RequestParam("swidth") double sienu_aukstis, 
-				@RequestParam("sheight") double sienu_ilgis,
-				@RequestParam("dwidth") double duru_aukstis, 
-				@RequestParam("dheight") double duru_ilgis,
-				@RequestParam("lwidth") double lango_aukstis, 
-				@RequestParam("lheight") double lango_ilgis,
-				Model model) {
-				
-			double kiekis=(sienu_aukstis*sienu_ilgis*4)-(duru_aukstis*duru_ilgis)-(lango_aukstis*lango_ilgis);
-			kiekis=Math.round(kiekis*100)/100.0;
-			
-			model.addAttribute("kiekis", kiekis);
-			return "rezultatas";
-		}
+	@GetMapping("/duom")
+	public String skaiciavimai() {
+		return "res";
+	}
+	
+	@PostMapping("/duom")
+	public String skaiciavimai(@RequestParam("d_ilgis") double d_ilgis, @RequestParam("s_ilgis") double s_ilgis,
+			@RequestParam("l_aukstis") double l_aukstis, @RequestParam("s_aukstis") double s_aukstis, 
+			@RequestParam("d_aukstis") double d_aukstis, @RequestParam("l_ilgis") double l_ilgis, 
+			@RequestParam("l_kiekis") Integer l_kiekis, @RequestParam("s_kiekis") Integer s_kiekis, 
+			@RequestParam("d_kiekis") Integer d_kiekis, Model model,
+			@RequestParam("t_ilgis") double t_ilgis, @RequestParam("t_aukstis") double t_aukstis) {
+	
+		double s_plotas = (s_ilgis*s_aukstis)*s_kiekis;
+		double d_plotas = (d_ilgis*d_aukstis)*d_kiekis;
+		double l_plotas = (l_ilgis*l_aukstis)*l_kiekis;
+		double kiekis = s_plotas-d_plotas-l_plotas;
+		double t_kiekis = kiekis / (t_ilgis*t_aukstis);
+		
+		model.addAttribute("kiekis", kiekis);
+		model.addAttribute("d_plotas", d_plotas);
+		model.addAttribute("l_plotas", l_plotas);
+		model.addAttribute("t_kiekis", t_kiekis);
+		return skaiciavimai();
+	}
 }
-
